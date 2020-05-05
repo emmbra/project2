bd90065a50b59d6f79482762f1b5e8c74ada1377
Emmett Brady <emmett.brady@gmail.com>
2020-05-05 13:47:52 -0700

    backend ready, now moving on to designing front end

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,6 +2,8 @@ const axios = require('axios');
 const connection = require('../config/connection');
 const movieQueries = require('../models/movies/movieQueries');
 
+// deleting/updating a specific movie - use reqparams
+// everything else use reqbody
 
 module.exports = {
   getAllMovies: async (req, res) => {
@@ -100,41 +102,44 @@ module.exports = {
       const { data } = await axios.get(`https://www.omdbapi.com/?t=${movieTitle}&apikey=${process.env.OMDB_API}`);
       console.log(data);
       res.status(200).json(data);
-    } catch (error) {
-      res.status(403).json({ error });
+    } catch (e) {
+      res.status(403).json({ e });
     }
   },
 
-  addRating: async (req, res) => {
-    const { rate } = req.body;
-    if (!rate) {
-      return res.json({ error: 'You must provide rate for movies '});
+  addMovie: async (req, res) => {
+    const { movieTitle } = req.body;
+    const { data } = await axios.get(`https://www.omdbapi.com/?t=${movieTitle}&apikey=${process.env.OMDB_API}`);
+    console.log('console1:', data.Title);
+    const { movieRating, movieSad, movieFunny, movieAction, movieRomance } = req.body;
+    const movieData = { ...req.body };
+    console.log('console2:', movieData);
+    if (!movieRating) {
+      return res.json({ error: 'You must provide a rating for the movie' });
     }
     try {
-      const [response] = await connection.query(movieQueries.addMovieRating, { rate });
-      console.log(response);
-      const [rate] = await connection.query(movieQueries.getMoviesByTitle, response.insertRate);
-      return res.json(rate[0]);
+      const [response] = await connection.query(movieQueries.addMovie, [data.Title, parseInt(data.Year), parseInt(movieRating), parseInt(movieSad), parseInt(movieFunny), parseInt(movieAction), parseInt(movieRomance)]);
+      const [movies] = await connection.query(movieQueries.getMovieById, response.insertId);
+      return res.json(movies[0]);
     } catch (e) {
       return res.status(403).json({ e });
     }
   },
-  
 
-  // addMovieRating: async (req, res) => {
-  //   const { movieRating } = req.body;
-  //   if (!movieRating) {
-  //     return res.json({ error: 'You must provide a rating for the movie '});
-  //   }
-  //   try {
-  // movieRating, movieComments, movieSad, movieFunny, movieAction, movieRomance
-  //     const [response] = await connection.query(movieQueries.addMovieRating, { text });
-  //     const [movies] = await connection.query(todoQueries.findTodoById, response.insertId);
-  //     return res.json(movies[0]);
-  //   } catch (e) {
-  //     return res.status(403).json({ e });
-  //   }
-  // },
+//   updateMovieRating: async (req, res) => {
+//     const { movieRating } = req.body;
+//     if (!movieRating) {
+//       return res.json({ error: 'You must provide a rating for the movie ' });
+//     }
+//     try {
+// // 'UPDATE movies SET movieRating = ? WHERE id = ?;';
+//       const [response] = await connection.query(movieQueries.updateMovieRating, { movieRating });
+//       const [movies] = await connection.query(movieQueries.getMovieById, response.insertId);
+//       return res.json(movies[0]);
+//     } catch (e) {
+//       return res.status(403).json({ e });
+//     }
+//   },
 
   // updateMovieRating: async (req, res) => {
   // },
