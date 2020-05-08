f6223dbcb0a52318d3fa97ad477ca1977d524729
Emmett Brady <emmett.brady@gmail.com>
2020-05-08 15:25:52 -0700

    grid system now works for movie poster rendering

--- client/src/components/RenderMovieListMoviesDB/index.js
+++ client/src/components/RenderMovieListMoviesDB/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
+import { Card } from 'semantic-ui-react'
 import SingleMovieCard from "../SingleMovieCard";
 import "./style.css";
 
 class RenderMovieListMoviesDB extends Component {
-
   componentDidMount() {
     this.props.getAllMovies();
   }
@@ -12,9 +12,13 @@ class RenderMovieListMoviesDB extends Component {
     if (!this.props.items.length) {
       return "";
     } else {
-      return this.props.items.map((movie, index) => {
-        return <SingleMovieCard key={index} movie={movie}/>
-      }); 
+      return (
+        <div className="cardContainer">
+          <Card.Group>
+            {this.props.items.map((movie, index) => <SingleMovieCard key={index} movie={movie} />)}
+          </Card.Group>
+        </div>
+      );
     }
   }
 }
